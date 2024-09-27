16ac33eed94f280bd033b20ad6215e889166cf39
wojciech.biernacki <w.biernacki03@gmail.com>
2024-09-27 14:03:47 +0200

    invoice-service

---
+++ src/test/java/com/Wojtek03/invoice_service/InvoiceServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Wojtek03.invoice_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvoiceServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
